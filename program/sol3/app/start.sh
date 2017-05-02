8ec6f9e0a4aed4c8fe2338cdf544b46b3423d6cb
AndresPineros <andres.pineros@hotmail.com>
2017-05-01 22:09:15 -0500

    solución 3 casi terminada. Falta mejorar dockerfile

--- program/sol3/app/start.sh
+++ program/sol3/app/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e  
 
-export pagina_text=${pagina_text:-"Default Hello World! Env var was not set! :O"}
+export text=${text:-"Default Hello World! Env var was not set! :O"}
 
 /usr/local/bin/confd -onetime -backend env
 
